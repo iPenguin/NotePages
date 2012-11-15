5c36137232c28d72661b63a3a8d8dff7c6a5cbf8
Brian Milco <bcmilco@gmail.com>
2012-11-15 13:26:43 -0800

    Update copyright and licensing notice GPLv2

--- src/appinfo.cpp
+++ src/appinfo.cpp
@@ -1,6 +1,21 @@
-/********************************************************\
-| Copyright (c) 2012 Brian C. Milco <bcmilco@gmail.com>  |
-\********************************************************/
+/*********************************************************************************************\
+    Note Pages - the note taking and cross reference application 
+    Copyright (c) 2012 Brian C. Milco <bcmilco AT gmail DOT com>       
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; version 2
+    of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+\*********************************************************************************************/
 #include "appinfo.h"
 
 #include <QMessageBox>
@@ -27,10 +42,12 @@ void AppInfo::helpAbout()
 {
 
     QString aboutInfo = QString(QObject::tr("<h1>%1</h1>"
-                                       "<p>Version: %2 (built on %3)</p>"
-                                       "<p>Copyright (c) 2012 Brian Milco</p>"
-                                       "<p>This software is a note taking application with the"
-                                        "ability to do cross-references like an online wiki.</p>")
+                                        "<p>Version: %2 (built on %3)</p>"
+                                        "<p>Copyright (c) 2012 Brian Milco</p>"
+                                        "<p>This software is a note taking application with the"
+                                        "ability to do cross-references like an online wiki.</p>"
+                                        "<p></p>"
+                                        "<p>%1 is distributed under the terms of the General Public License (GPL) version 2.</p>")
                                     .arg(qApp->applicationName())
                                     .arg(qApp->applicationVersion())
                                     .arg(appBuildInfo)
